4d62492fa452ec4c0b54f8adf71d82a2072d12e0
leandro <leandro.salustriano@hotmail.com>
2024-01-08 13:30:21 -0300

    added middle dto

--- src/controllers/ConfigurationController.ts
+++ src/controllers/ConfigurationController.ts
@@ -1,13 +1,16 @@
 import telebot from "telebot";
 import { ConfigurationService } from "../services/ConfigurationService";
 import { TelebotRouterDeco } from "../deco/TelebotRouterDeco";
+import { setRouterSwapDecoDTO } from "../deco/dto/setRouterSwapDecoDTO";
 
 const configurationService = new ConfigurationService();
 
 export class ConfigurationController{
 
+    @setRouterSwapDecoDTO()
     @TelebotRouterDeco("setrouter")
     setRouter(msg:any,telegram_bot:telebot,loading_message:any){
+      console.log("qtd")
         configurationService.setRouterSwap(msg,telegram_bot,loading_message).then((response) => {
             telegram_bot.editMessageText(
                 {
