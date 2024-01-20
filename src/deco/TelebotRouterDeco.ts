912a8c3b6bbb58e5e2d564d3c09cea3e62cba9c6
leandro <leandro.salustriano@hotmail.com>
2024-01-19 21:43:33 -0300

    reducao de carga

--- src/deco/TelebotRouterDeco.ts
+++ src/deco/TelebotRouterDeco.ts
@@ -10,33 +10,27 @@ export const telegram_bot = new telebot({
 export function TelebotRouterDeco(command: string) {
   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
-    
-    descriptor.value = async function (args: any) {      
-      return await originalMethod.call(this,args);
+
+    descriptor.value = async function (args: any) {
+      return await originalMethod.call(this, args);
     };
 
     telegram_bot.on(`/${command}`, async (msg, props) => {
-      let response;
-      const [, command, args] = msg.text.match( /\/(\w+)\s(.+)/) || [];
+      const [, command, args] = msg.text.match(/\/(\w+)\s(.+)/) || [];
 
       const loading_message = await telegram_bot.sendMessage(
-        msg.from.id,
-        "Executando ação, aguarde! (aguarde esta mensagem ser atualizada)" as any,
-        { replyToMessage: msg.message_id }
+        msg.from.id,"Aguarde ...",{ replyToMessage: msg.message_id }
       );
-        
-      response = await descriptor.value.call(null,args);       
 
-      if(response instanceof Error){
-        telegram_bot.editMessageText(
-          {
-            chatId: loading_message.chat.id,
-            messageId: loading_message.message_id,
-          },
-          response.message,
-          {parseMode:"markdown"}
-        );         
-      }
+      const response = await descriptor.value.call(null, args);
+
+      telegram_bot.editMessageText({
+        chatId: loading_message.chat.id,
+        messageId: loading_message.message_id,
+      },
+        response.message,
+        { parseMode: "markdown" }
+      );
 
       return response;
     });
